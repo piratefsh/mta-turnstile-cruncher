5f946b0f72501bbcbf2e187f7b52d3cb42a35ce8
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-01 12:46:44 -0400

    Add todo

--- mta-turnstile-cleaner.py
+++ mta-turnstile-cleaner.py
@@ -5,4 +5,13 @@ Clean up MTA Turnstile data generated from scraper:
 https://github.com/piratefsh/mta-turnstile-scraper
 """
 
+# todo
+# - rename ENTRIES and EXITS row to CUM_ENTRIES and CUM_EXITS
+# - for each entry, get entry and exists for that time period
+
+# - find stations with highest entry and exit traffic
+# -      stations with largest difference
+# -      line with highest and lowest ridership
+
+
 
