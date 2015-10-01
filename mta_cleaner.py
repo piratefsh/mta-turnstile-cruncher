bd9d360b09c118babfc56261368974ebb3d55cd5
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-01 16:16:17 -0400

    Find prev entry of time

--- mta_cleaner.py
+++ mta_cleaner.py
@@ -18,7 +18,7 @@ cursor = None
 
 # todo
 # - for each entry, get entry and exists for that time period
-def massage_db():
+def add_rows():
     # rename ENTRIES and EXITS row to CUM_ENTRIES and CUM_EXITS
     # get column headers and datatypes
     headers = cursor.execute('pragma table_info("entries")').fetchall()
@@ -45,9 +45,36 @@ def massage_db():
     cursor.execute(copy_query)
     connection.commit()
     
-    trace(copy_query)
-    return
+    return 
 
+def calc_entry_exits():
+    rows = cursor.execute('SELECT * FROM entries')
+    for row in rows:
+       get_prev_entry_by_timeslot(row)
+
+def get_prev_entry_by_timeslot(row):
+    # unroll data
+    # trace(row)
+    db_id, ca, unit, scp, station, line, _, date, time, _, cum_entries, cum_exits, _, _ = row
+    # if entry/exit is not 0
+    if cum_entries <= 0:
+        return None
+
+    # if there is an entry previous that matches
+    select = "UNIT,SCP,DATE,TIME,CUM_ENTRIES FROM entries"
+    prev = cursor.execute('SELECT %s WHERE id=? AND UNIT=? AND SCP=? AND DATE=?' % select, (str(db_id-1), unit, scp, date)).fetchone()
+    if prev:
+        # then find difference and log as entry/exit
+        punit, pscp, pdate, ptime, pcum_entries = prev
+        return (pcum_entries,) 
+        
+    else:
+        # find last time of previous day and compare to that 
+        prevs = cursor.execute('SELECT %s WHERE UNIT=? AND SCP=? AND DATE=? ORDER BY TIME' % select, (unit, scp, date)).fetchall()
+        if prev:
+            punit, pscp, pdate, ptime, pcum_entries = prevs[:1]
+            return (pcum_entries,)
+    return None
 
 def open_db(dbname):
    global connection, cursor
@@ -58,3 +85,31 @@ def open_db(dbname):
 # - find stations with highest entry and exit traffic
 # -      stations with largest difference
 # -      line with highest and lowest ridership
+
+def test():
+    open_db('test/mta-turnstile-2015-sept.db')
+    row = cursor.execute('SELECT * FROM entries WHERE id=?', (173178,)).fetchone()
+    prev = get_prev_entry_by_timeslot(row)
+    # trace(row, prev)
+    assert prev[0] == 2080793776 
+    
+    #id          CA          UNIT        SCP         STATION     LINENAME    DIVISION    DATE        TIME        DESC        CUM_ENTRIES  CUM_EXITS   ENTRIES     EXITS     
+    #----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  -----------  ----------  ----------  ----------
+    # 740286      R260        R205        01-05-01    149 ST-GR   245         IRT         09/04/2015  20:00:00    REGULAR     0            201 
+    row_entries_0 = cursor.execute('SELECT * FROM entries WHERE id=?', (740286,)).fetchone()
+    prev_0 = get_prev_entry_by_timeslot(row_entries_0)
+    assert prev_0 == None
+   
+    #id          CA          UNIT        SCP         STATION     LINENAME    DIVISION    DATE        TIME        DESC        CUM_ENTRIES  CUM_EXITS   ENTRIES     EXITS     
+    #----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  -----------  ----------  ----------  ----------
+    #623536      J034        R007        00-00-03    104 ST      JZ          BMT         09/03/2015  20:00:00    REGULAR     3917798      4409278                           
+    #623537      J034        R007        00-00-03    104 ST      JZ          BMT         09/04/2015  00:00:00    REGULAR     3917827      4409360        
+    row_time_00 = cursor.execute('SELECT * FROM entries WHERE id=?', (623537,)).fetchone()
+    prev_time_00 = get_prev_entry_by_timeslot(row_time_00)
+    assert prev_time_00 == 3917798
+
+    #774712      S101        R070        00-00-00    ST. GEORGE     1           SRT         08/29/2015  00:00:00    REGULAR     744553       137  
+    row_time_00_no_prev = cursor.execute('SELECT * FROM entries WHERE id=?', (774712,)).fetchone()
+    prev_time_00_none = get_prev_entry_by_timeslow(row_time_00_no_prev)
+    assert prev_time_00_none  == None
+    trace('test pass')
